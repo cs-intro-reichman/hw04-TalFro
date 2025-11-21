3de717fe494f8d3b819748d39486ad0a9ec7c314
Tal Froimovich <talfro@Tals-MacBook-Air.local>
2025-11-21 15:48:04 +0200

    hw4-TalFro

--- MyString.java
+++ MyString.java
@@ -18,13 +18,39 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String x = str.toLowerCase();
+        return x;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+       char [] S1 = new char[str1.length()];
+        char [] S2 = new char[str2.length()];
+
+        for(int i = 0; i < str1.length() ; i++){
+            S1[i] = str1.charAt(i);
+        }
+
+        for(int i = 0; i < str2.length() ; i++){
+            S2[i] = str2.charAt(i);
+        }
+
+        int x = 0;
+        for(int i = 0; i < S1.length; i++){
+            if(S1[i] == S2[0]){
+                x = i;
+                break;
+            }
+        }
+
+        for(int i = 0 ; i < S2.length ; i++){
+            if(S1[x] != S2[i]){
+                return false;
+            }
+            x++;
+        }
+
+
+        return true;
     }
 }
